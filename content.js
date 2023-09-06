54dc5cdc60da4d6a4c0134a6da579bfa7d02843d
elinelgp <elinelgp@gmail.com>
2023-09-06 15:19:03 +0200

    version dégueu

--- content.js
+++ content.js
@@ -1,7 +1,23 @@
 const button = document.getElementById("nav-search-submit-button");
 
 button.addEventListener("click", function(){ 
-    let input = document.getElementById("twotabsearchtextbox").value;
-    let link = `https://www.leboncoin.fr/recherche?text=${input}`;
-    alert(link); });
+    //let input = document.getElementById("twotabsearchtextbox").value;
+    //let link = `https://www.leboncoin.fr/recherche?text=${input}`;
+    togglePopup();
+   });
 
+function togglePopup() {
+    console.log("ok google");
+   
+// Crée l'élément p pour le paragraphe
+let paragraph = document.createElement("p");
+paragraph.textContent = "Cliquez sur le lien ci-dessous !";
+
+// Ajoute la popup dans le corps (body) du document
+document.body.appendChild(paragraph);
+console.log("hello")
+    // let popup = document.querySelector("#popup-overlay");
+    // popup.classList.toggle("open");
+}
+
+//document.querySelector(".popup-exit").addEventListener("click", togglePopup);
