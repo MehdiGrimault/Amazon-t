e83fc5653848ea5d38562586672dcaf6f6c49944
MahinGugu <mahinthan.gugananthan@gmail.com>
2023-09-11 14:20:10 +0200

    ajout google Lens filter

--- lens.js
+++ lens.js
@@ -1,2 +1,19 @@
-//document.querySelectorAll(`[data-action-url*=""]`)
-let results = document.querySelectorAll("[data-action-url]");
+function allowedUrl (url) {
+    for (const element of secondHandUrl) {
+      if (url.startsWith(element)) {
+      return true;
+      }
+    }
+    return false;
+  }
+  let results = document.querySelectorAll("[data-action-url]");
+  let secondHandUrl = ["https://www.leboncoin","https://www.backmarket","https://www.vinted","https://omaj"]
+  for (const result of results) {
+      let url = result.getAttribute("data-action-url");
+      if (!allowedUrl(url)) {
+        if (result.parentNode.tagName === "DIV" ) {
+          result.parentNode.style.display = "none";
+        }
+      }
+    }
+    results[0].parentNode.parentNode.parentNode.style = "display: flex; align-items: start";
